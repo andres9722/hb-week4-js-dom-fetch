534fd3450b42e169d2a67afb6d3dacbfba3c8740
andres9722 <andres_9722@outlook.com>
2018-01-18 20:21:28 -0500

    First commit

--- src/movies/index.js
+++ src/movies/index.js
@@ -2,6 +2,7 @@ import { Header } from './components/header/header.js'
 import { Movies } from './components/movies/movies.js'
 import { Grid } from './components/grid/grid.js'
 import Data from './components/data/movies-data.js'
+
 /* eslint-disable */
 const URL = 'https://api.myjson.com/bins/15mdhx'
 
@@ -15,14 +16,4 @@ Data.get(URL, data => {
                             grid.reset.bind(grid),
                             dataMovies)
 })
-
-
-/*const movies = dataMovies.map(movie => new Movies(movie))
-
-const grid = new Grid(container, movies, dataMovies)
-
-const header = new Header('.main-header', 
-                            grid.filterByCategory.bind(grid),
-                            grid.reset.bind(grid),
-                            dataMovies)*/
 /* eslint-enable */
